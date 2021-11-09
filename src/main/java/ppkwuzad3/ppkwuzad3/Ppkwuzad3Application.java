5dfe156a7188ca3410ce2f78bedfb72b8904f654
Mociej <maciekkocemba@gmail.com>
2021-11-09 12:49:52 +0100

    init

---
+++ src/main/java/ppkwuzad3/ppkwuzad3/Ppkwuzad3Application.java
@@ -0,0 +1,13 @@
+package ppkwuzad3.ppkwuzad3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppkwuzad3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppkwuzad3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ppkwuzad3/ppkwuzad3/Ppkwuzad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ppkwuzad3.ppkwuzad3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppkwuzad3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
