79eb12dd7709cd6ef1c2d4affe2b754f17bc38bf
Mociej <maciekkocemba@gmail.com>
2021-11-09 13:10:20 +0100

    check work

--- src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
+++ src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
@@ -1,8 +1,14 @@
 package ppkwuzad3.ppkwuzad3.Controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ppkwuzad2.ppkwuzad2.Controllers.StringApiController;
 
 @RestController
 public class FileSaverApiController {
-
+    @GetMapping("/{filetype}/{input}")
+    public String saveStatsToFile(@PathVariable String filetype, @PathVariable String input) {
+        StringApiController stringApiController = new StringApiController();
+        return stringApiController.stringAnalyse(input) + filetype;
+    }
 }
