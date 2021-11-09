79f4588182f44a4d4e0df5e90a97eaa4e3525cfc
Mociej <maciekkocemba@gmail.com>
2021-11-09 14:19:04 +0100

    json file

--- src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
+++ src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
@@ -1,9 +1,15 @@
 package ppkwuzad3.ppkwuzad3.Controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ppkwuzad2.ppkwuzad2.Controllers.StringApiController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class FileSaverApiController {
@@ -11,21 +17,40 @@ public class FileSaverApiController {
     public String saveStatsToFile(@PathVariable String filetype, @PathVariable String input) {
         StringApiController stringApiController = new StringApiController();
         String result = "";
+        String tmp = stringApiController.stringAnalyse(input);
+        tmp = tmp.replaceAll("\\s", "");
+        String lines[] = tmp.trim().split("<br/>");
+        ArrayList<String> data = new ArrayList<String>();
 
-        String lines[] = stringApiController.stringAnalyse(input).trim().split("<br/>");
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].length() > 0)
+                data.add(lines[i]);
+        }
 
-//        for (int i = 0; i < lines.length; i++) {
-//            System.out.println(lines[i]);
-//        }
+        Map<String, String> elements = new HashMap();
 
+        for (int i = 0; i < data.size(); i++) {
+            String keyvalue[] = data.get(i).trim().split(":");
+//            for (int j = 0; j < keyvalue.length; j++) {
+//                System.out.println(keyvalue[j]);
+//            }
+            elements.put(keyvalue[0], keyvalue[1]);
+        }
+        System.out.println("+++++++++++++++++");
         switch (filetype) {
             case "txt":
-                for (int i = 0; i < lines.length; i++) {
-                    result += lines[i] + "\n";
+                for (int i = 0; i < data.size(); i++) {
+                    result += data.get(i) + "\n";
                 }
                 break;
             case "json":
-                result = stringApiController.stringAnalyse(input) + filetype;
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    String json = objectMapper.writeValueAsString(elements);
+                    result = json;
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "xml":
                 result = stringApiController.stringAnalyse(input) + filetype;
---
+++ src/main/java/ppkwuzad3/ppkwuzad3/StringResultToMap.java
@@ -0,0 +1,11 @@
+package ppkwuzad3.ppkwuzad3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringResultToMap {
+    public Map<String, String> stringToMap(String str){
+        Map<String, String> map = new HashMap();
+        return map;
+    }
+}
