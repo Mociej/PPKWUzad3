7589d05d640a0384023ef4d3f93c0d29c206f298
Mociej <maciekkocemba@gmail.com>
2021-11-09 14:27:46 +0100

    fix txt formating

--- src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
+++ src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ppkwuzad2.ppkwuzad2.Controllers.StringApiController;
+import ppkwuzad3.ppkwuzad3.StringResultToMap;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,31 +18,21 @@ public class FileSaverApiController {
     public String saveStatsToFile(@PathVariable String filetype, @PathVariable String input) {
         StringApiController stringApiController = new StringApiController();
         String result = "";
-        String tmp = stringApiController.stringAnalyse(input);
-        tmp = tmp.replaceAll("\\s", "");
-        String lines[] = tmp.trim().split("<br/>");
-        ArrayList<String> data = new ArrayList<String>();
+        StringResultToMap stringResultToMap = new StringResultToMap();
+        Map<String, String> elements = stringResultToMap.stringToMap(stringApiController.stringAnalyse(input));
 
-        for (int i = 0; i < lines.length; i++) {
-            if (lines[i].length() > 0)
-                data.add(lines[i]);
-        }
 
-        Map<String, String> elements = new HashMap();
-
-        for (int i = 0; i < data.size(); i++) {
-            String keyvalue[] = data.get(i).trim().split(":");
-//            for (int j = 0; j < keyvalue.length; j++) {
-//                System.out.println(keyvalue[j]);
-//            }
-            elements.put(keyvalue[0], keyvalue[1]);
-        }
-        System.out.println("+++++++++++++++++");
         switch (filetype) {
             case "txt":
-                for (int i = 0; i < data.size(); i++) {
-                    result += data.get(i) + "\n";
+                String tmp = stringApiController.stringAnalyse(input);
+                tmp = tmp.replaceAll("\\s", "");
+                String lines[] = tmp.trim().split("<br/>");
+
+                for (int i = 0; i < lines.length; i++) {
+                    if (lines[i].length() > 0)
+                        result += lines[i] + "\n";
                 }
+
                 break;
             case "json":
                 ObjectMapper objectMapper = new ObjectMapper();
--- src/main/java/ppkwuzad3/ppkwuzad3/StringResultToMap.java
+++ src/main/java/ppkwuzad3/ppkwuzad3/StringResultToMap.java
@@ -1,11 +1,28 @@
 package ppkwuzad3.ppkwuzad3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StringResultToMap {
-    public Map<String, String> stringToMap(String str){
-        Map<String, String> map = new HashMap();
-        return map;
+    public Map<String, String> stringToMap(String tmp){
+        tmp = tmp.replaceAll("\\s", "");
+        String lines[] = tmp.trim().split("<br/>");
+        ArrayList<String> data = new ArrayList<String>();
+
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].length() > 0)
+                data.add(lines[i]);
+        }
+
+        Map<String, String> elements = new HashMap();
+
+        for (int i = 0; i < data.size(); i++) {
+            String keyvalue[] = data.get(i).trim().split(":");
+
+            elements.put(keyvalue[0], keyvalue[1]);
+        }
+
+        return elements;
     }
 }
