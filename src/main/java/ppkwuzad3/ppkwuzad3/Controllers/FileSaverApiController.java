10ad92a03c5388f34ca85a12b648dbb1cf80598e
Mociej <maciekkocemba@gmail.com>
2021-11-09 13:02:56 +0100

    add zad2 dependency

---
+++ src/main/java/ppkwuzad3/ppkwuzad3/Controllers/FileSaverApiController.java
@@ -0,0 +1,8 @@
+package ppkwuzad3.ppkwuzad3.Controllers;
+import org.springframework.web.bind.annotation.RestController;
+import ppkwuzad2.ppkwuzad2.Controllers.StringApiController;
+
+@RestController
+public class FileSaverApiController {
+
+}
